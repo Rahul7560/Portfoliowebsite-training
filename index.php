f3184fc703c79c9ff0250925745a08b7e4b875ea
Rahul7560 <rahulramesh125@gmail.com>
2020-09-04 11:34:24 +0530

    Second Commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Porfolio</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<!-- Navigation bar -->
+<div class ="navbar"  > 
+    <div class="mainnav" >
+        <div class="name" >
+            <h3> Rahul R </h3>
+            <h5> Developer </h5>
+
+
+        </div>
+        <div class ="navbar_button">
+
+        <a href="index.php"> <Button  >Home</Button>
+                <a href="#"> <Button >Resume</Button>
+                <a href="project.php"> <Button  >Project</Button>
+                </a>
+                <a href="#"> <Button  >Contact</Button> </a>
+
+
+        </div>
+        
+    </div>
+
+
+
+
+
+</div>
+
+<!-- Main content -->
+<div class ="main">
+    <div class="img">
+        <img  src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"  />
+    </div>
+
+    <div class="hello">
+        <h1> Hello <h1>
+        <h3> a Bit about me </h3>
+            <div class="resumebutton">
+                <Button class="button1" > My Resume</Button>
+                <Button class="button2"> My Work</Button>
+                <Button class="button3"> My Skills</Button>
+           </div>
+        <p> I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story and let your users get to know you. </p>
+
+    </div>
+
+
+
+
+
+ </div>
+
+
+<!-- footer -->
+<div class ="footer"> 
+    <div > 
+        <div class="foot" >
+            <h3> Call </h3>
+            <p>12342345</p>
+            <p>12342345</p>
+
+        </div>
+    </div>
+
+
+    <div > 
+       <div class="foot">
+            <h3> Contact </h3>
+            <p>rahulramesh7560@gmail.com</p>
+
+    
+        </div>
+    </div>
+
+
+    <div > 
+        <div class="foot">
+            <h3> Follow </h3>
+            <p>12342345</p>
+            <p>12342345</p>
+
+        </div>
+    </div>
+
+
+    <div > 
+        <div class="foot">
+            <h3> Copyright </h3>
+            <p>Created by</p>
+            <p>Rahul</p>
+
+        </div>
+    </div>
+    
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
---
+++ project.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="project.css">
+
+</head>
+<body>
+    <!-- Navigation bar -->
+    <div class ="navbar"  > 
+    <div class="mainnav" >
+        <div class="name" >
+            <h3> Rahul R </h3>
+            <h5> Developer </h5>
+
+
+        </div>
+        <div class ="navbar_button">
+
+        <a href="index.php"> <Button  >Home</Button>
+                <a href="#"> <Button >Resume</Button>
+                <a href="project.php"> <Button  >Project</Button>
+                </a>
+                <a href="#"> <Button  >Contact</Button>
+</a>
+
+
+        </div>
+        
+    </div>
+
+
+
+
+
+</div>
+
+
+<!-- project -->
+<div class="project">
+    <h1> LATEST PROJECT </h1>
+    <hr>
+    <div class="projectdiv">
+        <!-- image -->
+        <div class="image_project"> 
+            <img  src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"  />
+        </div>
+        <!-- details -->
+
+        <div class="projectdetail" > 
+            <div> <h3> Project 1 </h3></div>
+            <hr>
+            <p>I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story about yourself
+            </p>
+
+        </div>
+
+
+    </div>
+</div>
+<!-- 2nd -->
+<div class="project">
+  
+    <hr>
+    <div class="projectdiv">
+        <!-- image -->
+        <div class="image_project"> 
+            <img  src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"  />
+        </div>
+        <!-- details -->
+
+        <div class="projectdetail" > 
+            <div> <h3> Project 2 </h3></div>
+            <hr>
+            <p>I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story about yourself
+            </p>
+
+        </div>
+
+
+    </div>
+</div>
+<!-- footer -->
+<div class ="footer"> 
+    <div > 
+        <div class="foot" >
+            <h3> Call </h3>
+            <p>12342345</p>
+            <p>12342345</p>
+
+        </div>
+    </div>
+
+
+    <div > 
+       <div class="foot">
+            <h3> Contact </h3>
+            <p>rahulramesh7560@gmail.com</p>
+
+    
+        </div>
+    </div>
+
+
+    <div > 
+        <div class="foot">
+            <h3> Follow </h3>
+            <p>12342345</p>
+            <p>12342345</p>
+
+        </div>
+    </div>
+
+
+    <div > 
+        <div class="foot">
+            <h3> Copyright </h3>
+            <p>Created by</p>
+            <p>Rahul</p>
+
+        </div>
+    </div>
+    
+
+
+
+</body>
+</html>
